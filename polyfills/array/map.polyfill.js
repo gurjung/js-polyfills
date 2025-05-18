30477582a75b3fa27c4da2ee2fa6b9a2e867fbe6
gurjung <gurjung997@gmail.com>
2025-05-18 19:02:42 +0530

    add: polyfill for map with thisArg & sparse support

---
+++ polyfills/array/map.polyfill.js
@@ -0,0 +1,23 @@
+/**
+ * Polyfill for Array.prototype.map
+ * Supports:
+ * - thisArg binding
+ * - Sparse arrays (skips holes)
+ * - Type checking for callback
+ */
+
+Array.prototype.myMap = function (callback, thisArg) {
+  if (typeof callback !== 'function') {
+    throw new Error('Callback is not a function');
+  }
+
+  const tempArr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (i in this) {
+      tempArr.push(callback.call(thisArg, this[i], i, this));
+    }
+  }
+
+  return tempArr;
+};
